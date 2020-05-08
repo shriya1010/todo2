29ccd985f23824a966d1924f1be2fa7c95868b75
Shriya <shriyasawant89@gmail.com>
2020-05-08 17:54:57 +0530

    commiyed

---
+++ components/main.js
@@ -0,0 +1,63 @@
+/* Vue.component('app', {
+  data: function () {
+    return {      
+    }
+  },
+  template: '<div>   <input type="text">   <button class="btn btn-primary" >Add</button>  </div>  '
+})
+
+Vue.component('list', {
+  data: function () {
+    return {
+      "tasks": ["study","play","code"],
+      
+    }
+  },
+  template: '<div> <ul class="nav flex-column"><li class="nav-item" v-for="task in tasks" > <input type="checkbox">       {{ task }}<button  class="btn btn-primary float-right"  > &times;</button></li></ul> </div>  '
+})
+*/
+
+//{title:"assignment steps", items:[{ name: "Make a vue instance", checked: false},{ name: "Make a vue instance", checked: false}]}
+var comp2 = {
+  data: function () {
+    return {
+      //"tasks": ["study","play","code"],
+     "tasks":
+      [
+        {
+          "value":"code",
+          "checked":false
+        },
+        {
+          "value":"play",
+          "checked":true
+        },
+        {
+          "value":"study",
+          "checked":false
+        }
+
+      ]
+      
+    }
+  },
+  
+  template: '<div><ul class="nav flex-column"><li class="nav-item" v-for="task in tasks" > <input type="checkbox" v-model="checked">{{ task.value }}<button  class="btn btn-primary float-right"  > &times;</button></li></ul> <input type="text">   <button class="btn btn-primary" >Add</button></div>'
+
+  }
+var comp1= {
+  data: function () {
+    return {      
+    }
+  },
+  components:{ list:comp2},
+  template: '<div id="main"><list></list></div>  '
+  }
+
+new Vue({ 
+  el: '#maincomponent', 
+  components:{
+    app:comp1
+  }
+
+})
