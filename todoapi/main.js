bb7551adccea4ad0cddb54431b7544118a16e5ea
Shriya <shriyasawant89@gmail.com>
2020-05-09 17:51:40 +0530

    apicommit

---
+++ todoapi/main.js
@@ -0,0 +1,35 @@
+var comp2 = {
+  data: function () {
+    return {
+    }
+  },
+  props:['name'],
+  template: '<li class="list_name"><input type="checkbox" v-model="name.checked">{{name.value}} <button  class="btn btn-primary btn-sm float-right"  > &times;</button></li>'
+  }
+var comp1= {
+  
+  data: function () {
+    return { 
+    }
+  },
+  components:{ list:comp2},
+  props:['tasks'],
+  template: '<div id="main"><ul class="nav flex-column"> <list v-for="task in tasks" v-bind:name="task" ></list></ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
+  }
+
+new Vue({ 
+  el: '#maincomponent', 
+  data:{
+    name:"",
+    info:""
+  },
+  components:{
+    app:comp1
+  },
+  mounted () {
+    axios
+      .get(' http://localhost:3000/tasks')
+      .then(response => (this.info = response))
+  }
+
+})
