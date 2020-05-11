cae2ead21b10c529d7fbf959f5e1a7f6c7ee93f1
Shriya <shriyasawant89@gmail.com>
2020-05-11 17:46:12 +0530

    nextcommit

--- todoapi/main.js
+++ todoapi/main.js
@@ -1,23 +1,31 @@
-var comp2 = {
+var comp3 = {
   data: function () {
     return {
     }
   },
   props:['name'],
-  template: '<li class="list_name"><input type="checkbox" v-model="name.checked">{{name.value}} <button  class="btn btn-primary btn-sm float-right"  > &times;</button></li>'
+  template: '<li id="main"><input type="checkbox" v-model="name.checked">{{name.value}} <button  class="btn btn-primary btn-sm float-right"  > &times;</button></li>'
   }
+var comp2= {
+  
+  data: function () {
+    return { 
+    }
+  },
+  components:{ list:comp3},
+  props:['tasks'],
+  template: '<div ><ul class="nav flex-column"> <list v-for="task in tasks" v-bind:name="task" ></list></ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
+  
+}
 var comp1= {
   
   data: function () {
     return { 
     }
   },
-  components:{ list:comp2},
-  props:['tasks'],
-  template: '<div id="main"><ul class="nav flex-column"> <list v-for="task in tasks" v-bind:name="task" ></list></ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
-  
-  //template: '<div id="main"><ul class="nav flex-column"> {{tasks.title}}</ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
-  
+  components:{ app:comp2},
+  props:['info'],
+  template: '<div>  <p v-for="data in info" >{{data.title}} <button  class="btn btn-primary btn-sm float-right" v-on:click="data.show=!data.show" >View</button> <app v-show="data.show" v-bind:tasks="data.work"></app> </p> </div>'
 }
 new Vue({ 
   el: '#maincomponent', 
@@ -27,7 +35,7 @@ new Vue({
     data:""
   },
   components:{
-    app:comp1
+    titlelist:comp1
   },
   mounted () {
     axios
