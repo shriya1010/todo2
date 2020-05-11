08ade9168a279a7a6cddf13622c1a352c6359f63
Shriya <shriyasawant89@gmail.com>
2020-05-11 15:40:05 +0530

    commit

--- todoapi/main.js
+++ todoapi/main.js
@@ -15,13 +15,16 @@ var comp1= {
   components:{ list:comp2},
   props:['tasks'],
   template: '<div id="main"><ul class="nav flex-column"> <list v-for="task in tasks" v-bind:name="task" ></list></ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
-  }
-
+  
+  //template: '<div id="main"><ul class="nav flex-column"> {{tasks.title}}</ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
+  
+}
 new Vue({ 
   el: '#maincomponent', 
   data:{
     name:"",
-    info:""
+    info:"",
+    data:""
   },
   components:{
     app:comp1
@@ -31,5 +34,4 @@ new Vue({
       .get(' http://localhost:3000/tasks')
       .then(response => (this.info = response))
   }
-
 })
