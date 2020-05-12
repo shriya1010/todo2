465c58815136d209d38281fd87424bbc0dd08cc8
Shriya <shriyasawant89@gmail.com>
2020-05-12 18:47:37 +0530

    nextcommit

--- vuerouter/main.js
+++ vuerouter/main.js
@@ -4,7 +4,7 @@ var comp3 = {
     }
   },
   props:['name'],
-  template: '<li id="main"><input type="checkbox" v-model="name.checked">{{name.value}} <button  class="btn btn-primary btn-sm float-right"  > &times;</button></li>'
+  template: '<li id="main">comp3<input type="checkbox" v-model="name.checked">{{name.value}} <button  class="btn btn-primary btn-sm float-right"  > &times;</button></li>'
   }
 var comp2= { 
   data: function () {
@@ -13,8 +13,10 @@ var comp2= {
   },
   components:{ list:comp3},
   props:['tasks'],
-  template: '<div ><ul class="nav flex-column"> <list v-for="task in tasks" v-bind:name="task" ></list></ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
-  }
+  //template: '<div ><ul class="nav flex-column"> fdgg<list v-for="task in tasks" v-bind:name="task" ></list></ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
+  template: '<div ><ul class="nav flex-column">comp2{{tasks}}</ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
+    
+}
 var comp1= {  
   data: function () {
     return { 
@@ -23,15 +25,29 @@ var comp1= {
   components:{ app:comp2},
   props:['info'],
   //template: '<div> <p v-for="data in info" ><router-link to="/comp2">{{data.title}} </router-link> <app v-show="data.show" v-bind:tasks="data.work"></app> </p>  <router-view></router-view>   </div>'
-  template: '<div> <p v-for="data in info" ><router-link to="/comp2/data.work">{{data.title}} </router-link>  </p>  <router-view></router-view>   </div>'
+  template: "<div> <p v-for='data in info' v-bind:info='data.work'>  <router-link to=/comp2/>{{ $route.params.info }}{{data.title}}</router-link>    </p>  <router-view></router-view>   </div>"
 
 }
 const routes = [
-  { path: '/comp2/:tasks', component: comp2 , 
+  {
+    path: '/comp2/', 
+    name:'comp2',
+  component: comp2 ,
+  props: true ,
+  
+  children: [
+    {
+      // UserProfile will be rendered inside User's <router-view>
+      // when /user/:id/profile is matched
+      path: '*',
+      component: comp3
+    } 
+  ]
   }
 ]
 const router = new VueRouter({
-  routes // short for `routes: routes`
+    routes // short for `routes: routes`
+    
 })
 new Vue({   
   el: '#maincomponent', 
