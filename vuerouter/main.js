0f67dcf762cd5705e93e5c8fdb0372235af87002
Shriya <shriyasawant89@gmail.com>
2020-05-13 12:03:23 +0530

    nextcommit

--- vuerouter/main.js
+++ vuerouter/main.js
@@ -4,52 +4,39 @@ var comp3 = {
     }
   },
   props:['name'],
-  template: '<li id="main">comp3<input type="checkbox" v-model="name.checked">{{name.value}} <button  class="btn btn-primary btn-sm float-right"  > &times;</button></li>'
+  template: '<li id="main"><input type="checkbox" v-model="name.checked">{{name.value}} <button  class="btn btn-primary btn-sm float-right"  > &times;</button></li>'
   }
-var comp2= { 
+var comp2= {
+  
   data: function () {
     return { 
     }
   },
   components:{ list:comp3},
   props:['tasks'],
-  //template: '<div ><ul class="nav flex-column"> fdgg<list v-for="task in tasks" v-bind:name="task" ></list></ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
-  template: '<div ><ul class="nav flex-column">comp2{{tasks}}</ul><input type="text">   <button class="btn btn-primary" >Add</button></div>  '
-    
+  template: '<div><ul class="nav flex-column"> <list v-for="task in tasks" v-bind:name="task" ></list></ul><input type="text"> <button class="btn btn-primary" >Add</button></div>  '
+  
 }
-var comp1= {  
+var comp1= {
+  
   data: function () {
     return { 
     }
   },
   components:{ app:comp2},
   props:['info'],
-  //template: '<div> <p v-for="data in info" ><router-link to="/comp2">{{data.title}} </router-link> <app v-show="data.show" v-bind:tasks="data.work"></app> </p>  <router-view></router-view>   </div>'
-  template: "<div> <p v-for='data in info' v-bind:info='data.work'>  <router-link to=/comp2/>{{ $route.params.info }}{{data.title}}</router-link>    </p>  <router-view></router-view>   </div>"
+  //template: '<div>  <p v-for="data in info"><router-link to="/next">{{data.title}}</router-link><app v-bind:tasks="data.work"></app> </p> </div>'
+  template: '<div>  <p v-for="data in info"><router-link to="/next">{{data.title}}</router-link></p>  </div>'
 
 }
 const routes = [
-  {
-    path: '/comp2/', 
-    name:'comp2',
-  component: comp2 ,
-  props: true ,
+  { path: '/next', component: comp2 },
   
-  children: [
-    {
-      // UserProfile will be rendered inside User's <router-view>
-      // when /user/:id/profile is matched
-      path: '*',
-      component: comp3
-    } 
-  ]
-  }
 ]
 const router = new VueRouter({
-    routes // short for `routes: routes`
-    
+  routes // short for `routes: routes`
 })
-new Vue({   
+new Vue({ 
   el: '#maincomponent', 
   data:{
     name:"",
@@ -64,5 +51,5 @@ new Vue({
       .get(' http://localhost:3000/tasks')
       .then(response => (this.info = response))
   },
-   router
+  router
 }).$mount('#maincomponent')
