9be79052a2d5767b53777cf395d3abe3691acc43
Shriya <shriyasawant89@gmail.com>
2020-05-13 16:08:37 +0530

    nextcommit

--- vuerouter/main.js
+++ vuerouter/main.js
@@ -9,25 +9,37 @@ var comp3 = {
 var comp2= {
   
   data: function () {
-    return { 
+    return { tasks:"",
+    info:""
     }
   },
+  props:['title'],
   components:{ list:comp3},
-  props:['tasks'],
-  template: '<div><ul class="nav flex-column"> <list v-for="task in tasks" v-bind:name="task" ></list></ul><input type="text"> <button class="btn btn-primary" >Add</button></div>  '
-  
+  mounted () {
+    axios
+      .get(' http://localhost:3000/tasks?title=shopping')
+      .then(response => (this.info = response))
+  },
+  template: '<div><ul class="nav flex-column" v-for="tasks in info.data"><list v-for="task in tasks.work" v-bind:name="task" ></list></ul><input type="text"> <button class="btn btn-primary" >Add</button></div>  '
+  //http://localhost:3000/tasks?title="title"
 }
 var comp1= {
   
   data: function () {
     return { 
+      //list:"",
+      info:""
     }
   },
   components:{ app:comp2},
-  props:['info'],
+  mounted () {
+    axios
+      .get(' http://localhost:3000/tasks')
+      .then(response => (this.info = response))
+  },
   //template: '<div>  <p v-for="data in info"><router-link to="/next">{{data.title}}</router-link><app v-bind:tasks="data.work"></app> </p> </div>'
-  template: '<div> <p v-for="data in info"><router-link to="/next">{{data.title}}</router-link></p>  </div>'
-
+  //template: '<div>bgh <p v-for="data in info">njn<router-link to="/next">{{data.title}}{{info}}</router-link></p>  </div>'
+  template:'<div><p v-for="list in info.data" v-bind:title="list.title"><router-link to="/next">{{list.title}}</router-link></p></div>'
 }
 const routes = [
   
@@ -43,7 +55,8 @@ new Vue({
   data:{
     name:"",
     info:"",
-    data:""
+    data:"" // how will comp1 get this data now? you cannot use props to send data through router view.. also if u could.. all components dont need that data.. so what would you do? think about it 
+
   },
   components:{
     titlelist:comp1
